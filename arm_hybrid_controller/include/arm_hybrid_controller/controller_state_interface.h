31e689a3e6e48cb14276abbb36397ed1b583cb21
1moule <1961466188@qq.com>
2023-11-06 00:44:46 +0800

    Modify include files' paths and add format files.

--- arm_hybrid_controller/include/arm_hybrid_controller/controller_state_interface.h
+++ arm_hybrid_controller/include/arm_hybrid_controller/controller_state_interface.h
@@ -20,153 +20,156 @@ namespace arm_hybrid_controller
 {
 struct TimeData
 {
-    TimeData() : time(0.0), period(0.0), uptime(0.0) {}
-    ros::Time     time;   ///< Time of last update cycle
-    ros::Duration period; ///< Period of last update cycle
-    ros::Time     uptime; ///< Controller uptime. Set to zero at every restart.
+  TimeData() : time(0.0), period(0.0), uptime(0.0)
+  {
+  }
+  ros::Time time;        ///< Time of last update cycle
+  ros::Duration period;  ///< Period of last update cycle
+  ros::Time uptime;      ///< Controller uptime. Set to zero at every restart.
 };
 class ControllerStateInterface
 {
 public:
-    void init(ros::NodeHandle& controller_nh,std::vector<std::string> joint_names)
-    {
-        double state_publish_rate = 50.0;
-        controller_nh.getParam("state_publish_rate", state_publish_rate);
-        state_publisher_period_ = ros::Duration(1.0 / state_publish_rate);
-        state_publisher_.reset(new realtime_tools::RealtimePublisher<control_msgs::JointTrajectoryControllerState>(controller_nh, "state", 1));
+  void init(ros::NodeHandle& controller_nh, std::vector<std::string> joint_names)
+  {
+    double state_publish_rate = 50.0;
+    controller_nh.getParam("state_publish_rate", state_publish_rate);
+    state_publisher_period_ = ros::Duration(1.0 / state_publish_rate);
+    state_publisher_.reset(
+        new realtime_tools::RealtimePublisher<control_msgs::JointTrajectoryControllerState>(controller_nh, "state", 1));
 
-        // Preeallocate resources
-        num_hw_joints_ = static_cast<int>(joint_names.size());
-        current_state_       = Segment::State(num_hw_joints_);
-        old_desired_state_   = Segment::State(num_hw_joints_);
-        desired_state_       = Segment::State(num_hw_joints_);
-        state_error_         = Segment::State(num_hw_joints_);
-        desired_joint_state_ = Segment::State(1);
-        state_joint_error_   = Segment::State(1);
+    // Preeallocate resources
+    num_hw_joints_ = static_cast<int>(joint_names.size());
+    current_state_ = Segment::State(num_hw_joints_);
+    old_desired_state_ = Segment::State(num_hw_joints_);
+    desired_state_ = Segment::State(num_hw_joints_);
+    state_error_ = Segment::State(num_hw_joints_);
+    desired_joint_state_ = Segment::State(1);
+    state_joint_error_ = Segment::State(1);
 
-        state_publisher_->lock();
-        state_publisher_->msg_.joint_names = joint_names;
-        state_publisher_->msg_.desired.positions.resize(num_hw_joints_);
-        state_publisher_->msg_.desired.velocities.resize(num_hw_joints_);
-        state_publisher_->msg_.desired.accelerations.resize(num_hw_joints_);
-        state_publisher_->msg_.actual.positions.resize(num_hw_joints_);
-        state_publisher_->msg_.actual.velocities.resize(num_hw_joints_);
-        state_publisher_->msg_.error.positions.resize(num_hw_joints_);
-        state_publisher_->msg_.error.velocities.resize(num_hw_joints_);
-        state_publisher_->unlock();
-    }
-    void initTimeData(const ros::Time& time)
+    state_publisher_->lock();
+    state_publisher_->msg_.joint_names = joint_names;
+    state_publisher_->msg_.desired.positions.resize(num_hw_joints_);
+    state_publisher_->msg_.desired.velocities.resize(num_hw_joints_);
+    state_publisher_->msg_.desired.accelerations.resize(num_hw_joints_);
+    state_publisher_->msg_.actual.positions.resize(num_hw_joints_);
+    state_publisher_->msg_.actual.velocities.resize(num_hw_joints_);
+    state_publisher_->msg_.error.positions.resize(num_hw_joints_);
+    state_publisher_->msg_.error.velocities.resize(num_hw_joints_);
+    state_publisher_->unlock();
+  }
+  void initTimeData(const ros::Time& time)
+  {
+    // Update time data
+    TimeData time_data;
+    time_data.time = time;
+    time_data.uptime = ros::Time(0.0);
+    time_data_.initRT(time_data);
+    last_state_publish_time_ = time_data.uptime;
+  }
+  void updateTimeData(const ros::Time& time, const ros::Duration& period)
+  {
+    old_time_data_ = *(time_data_.readFromRT());
+    TimeData time_data;
+    time_data.time = time;                              // Cache current time
+    time_data.period = period;                          // Cache current control period
+    time_data.uptime = old_time_data_.uptime + period;  // Update controller uptime
+    time_data_.writeFromNonRT(time_data);
+  }
+  void initDesiredState(const std::vector<hardware_interface::JointStateHandle> jnt_state)
+  {
+    for (int i = 0; i < num_hw_joints_; ++i)
     {
-        // Update time data
-        TimeData time_data;
-        time_data.time  = time;
-        time_data.uptime = ros::Time(0.0);
-        time_data_.initRT(time_data);
-        last_state_publish_time_ = time_data.uptime;
+      desired_state_.position[i] = jnt_state[i].getPosition();
+      desired_state_.velocity[i] = jnt_state[i].getVelocity();
     }
-    void updateTimeData(const ros::Time& time,const ros::Duration& period)
+  }
+  void updateCurrentState(const std::vector<hardware_interface::JointStateHandle> jnt_state)
+  {
+    for (int i = 0; i < num_hw_joints_; ++i)
     {
-        old_time_data_ = *(time_data_.readFromRT());
-        TimeData time_data;
-        time_data.time   = time;                                     // Cache current time
-        time_data.period = period;                                   // Cache current control period
-        time_data.uptime = old_time_data_.uptime + period; // Update controller uptime
-        time_data_.writeFromNonRT(time_data);
+      current_state_.position[i] = jnt_state[i].getPosition();
+      current_state_.velocity[i] = jnt_state[i].getVelocity();
     }
-    void initDesiredState(const std::vector<hardware_interface::JointStateHandle> jnt_state)
+  }
+  void updateErrorState()
+  {
+    for (int i = 0; i < num_hw_joints_; ++i)
     {
-        for (int i = 0; i < num_hw_joints_; ++i)
-        {
-            desired_state_.position[i] = jnt_state[i].getPosition();
-            desired_state_.velocity[i] = jnt_state[i].getVelocity();
-        }
+      state_error_.position[i] = desired_state_.position[i] - current_state_.position[i];
+      state_error_.velocity[i] = desired_state_.velocity[i] - current_state_.velocity[i];
     }
-    void updateCurrentState(const std::vector<hardware_interface::JointStateHandle> jnt_state)
-    {
-        for (int i = 0; i < num_hw_joints_; ++i)
-        {
-            current_state_.position[i] = jnt_state[i].getPosition();
-            current_state_.velocity[i] = jnt_state[i].getVelocity();
-        }
-    }
-    void updateErrorState()
-    {
-        for (int i = 0; i < num_hw_joints_; ++i)
-        {
-            state_error_.position[i] = desired_state_.position[i] - current_state_.position[i];
-            state_error_.velocity[i] = desired_state_.velocity[i] - current_state_.velocity[i];
-        }
-    }
+  }
 
-    template<class Trajectory>
-    void updateDesiredStates(const ros::Time& sample_time, const Trajectory* const traj)
+  template <class Trajectory>
+  void updateDesiredStates(const ros::Time& sample_time, const Trajectory* const traj)
+  {
+    old_desired_state_ = desired_state_;
+    for (int joint_index = 0; joint_index < num_hw_joints_; ++joint_index)
     {
-        old_desired_state_ = desired_state_;
-        for (int joint_index = 0; joint_index < num_hw_joints_; ++joint_index)
-        {
-            const auto segment = trajectory_interface::sample( (*traj)[joint_index], sample_time.toSec(), desired_joint_state_);
+      const auto segment =
+          trajectory_interface::sample((*traj)[joint_index], sample_time.toSec(), desired_joint_state_);
 
-            desired_state_.position[joint_index] = desired_joint_state_.position[0];
-            desired_state_.velocity[joint_index] = desired_joint_state_.velocity[0];
-            desired_state_.acceleration[joint_index] = desired_joint_state_.acceleration[0];
+      desired_state_.position[joint_index] = desired_joint_state_.position[0];
+      desired_state_.velocity[joint_index] = desired_joint_state_.velocity[0];
+      desired_state_.acceleration[joint_index] = desired_joint_state_.acceleration[0];
 
-            state_error_.position[joint_index] = desired_joint_state_.position[0] - current_state_.position[joint_index];
-            state_error_.velocity[joint_index] = desired_joint_state_.velocity[0] - current_state_.velocity[joint_index];
-            state_error_.acceleration[joint_index] = 0.0;
+      state_error_.position[joint_index] = desired_joint_state_.position[0] - current_state_.position[joint_index];
+      state_error_.velocity[joint_index] = desired_joint_state_.velocity[0] - current_state_.velocity[joint_index];
+      state_error_.acceleration[joint_index] = 0.0;
 
-            if (joint_index == 0)
-            {
-                const auto time_from_start = segment->timeFromStart();
-                current_state_.time_from_start = sample_time.toSec() - segment->startTime() + time_from_start;
-                desired_state_.time_from_start = time_from_start;
-                state_error_.time_from_start = desired_state_.time_from_start - current_state_.time_from_start;
-            }
-        }
+      if (joint_index == 0)
+      {
+        const auto time_from_start = segment->timeFromStart();
+        current_state_.time_from_start = sample_time.toSec() - segment->startTime() + time_from_start;
+        desired_state_.time_from_start = time_from_start;
+        state_error_.time_from_start = desired_state_.time_from_start - current_state_.time_from_start;
+      }
     }
-    void update(const ros::Time& time,const std::vector<hardware_interface::JointStateHandle> jnt_state)
+  }
+  void update(const ros::Time& time, const std::vector<hardware_interface::JointStateHandle> jnt_state)
+  {
+    updateCurrentState(jnt_state);
+    publishState(time);
+  }
+  void publishState(const ros::Time& time)
+  {
+    // Check if it's time to publish
+    if (!state_publisher_period_.isZero() && last_state_publish_time_ + state_publisher_period_ < time)
     {
-        updateCurrentState(jnt_state);
-        publishState(time);
+      if (state_publisher_ && state_publisher_->trylock())
+      {
+        last_state_publish_time_ += state_publisher_period_;
+
+        state_publisher_->msg_.header.stamp = time_data_.readFromRT()->time;
+        state_publisher_->msg_.desired.positions = desired_state_.position;
+        state_publisher_->msg_.desired.velocities = desired_state_.velocity;
+        state_publisher_->msg_.desired.accelerations = desired_state_.acceleration;
+        state_publisher_->msg_.desired.time_from_start = ros::Duration(desired_state_.time_from_start);
+        state_publisher_->msg_.actual.positions = current_state_.position;
+        state_publisher_->msg_.actual.velocities = current_state_.velocity;
+        state_publisher_->msg_.actual.time_from_start = ros::Duration(current_state_.time_from_start);
+        state_publisher_->msg_.error.positions = state_error_.position;
+        state_publisher_->msg_.error.velocities = state_error_.velocity;
+        state_publisher_->msg_.error.time_from_start = ros::Duration(state_error_.time_from_start);
+
+        state_publisher_->unlockAndPublish();
+      }
     }
-    void publishState(const ros::Time& time)
-    {
-        // Check if it's time to publish
-        if (!state_publisher_period_.isZero() && last_state_publish_time_ + state_publisher_period_ < time)
-        {
-            if (state_publisher_ && state_publisher_->trylock())
-            {
-                last_state_publish_time_ += state_publisher_period_;
+  }
+  typedef joint_trajectory_controller::JointTrajectorySegment<trajectory_interface::QuinticSplineSegment<double>> Segment;
+  Segment::State current_state_;
+  Segment::State desired_state_;
+  Segment::State old_desired_state_;
+  Segment::State state_error_;
+  Segment::State desired_joint_state_;
+  Segment::State state_joint_error_;
 
-                state_publisher_->msg_.header.stamp          = time_data_.readFromRT()->time;
-                state_publisher_->msg_.desired.positions     = desired_state_.position;
-                state_publisher_->msg_.desired.velocities    = desired_state_.velocity;
-                state_publisher_->msg_.desired.accelerations = desired_state_.acceleration;
-                state_publisher_->msg_.desired.time_from_start = ros::Duration(desired_state_.time_from_start);
-                state_publisher_->msg_.actual.positions      = current_state_.position;
-                state_publisher_->msg_.actual.velocities     = current_state_.velocity;
-                state_publisher_->msg_.actual.time_from_start = ros::Duration(current_state_.time_from_start);
-                state_publisher_->msg_.error.positions       = state_error_.position;
-                state_publisher_->msg_.error.velocities      = state_error_.velocity;
-                state_publisher_->msg_.error.time_from_start = ros::Duration(state_error_.time_from_start);
-
-                state_publisher_->unlockAndPublish();
-            }
-        }
-    }
-    typedef joint_trajectory_controller::JointTrajectorySegment<trajectory_interface::QuinticSplineSegment<double>> Segment;
-    Segment::State current_state_;
-    Segment::State desired_state_;
-    Segment::State old_desired_state_;
-    Segment::State state_error_;
-    Segment::State desired_joint_state_;
-    Segment::State state_joint_error_;
-
-    int num_hw_joints_;
-    ros::Duration state_publisher_period_;
-    TimeData old_time_data_;
-    realtime_tools::RealtimeBuffer<TimeData> time_data_;
-    ros::Time     last_state_publish_time_;
-    std::unique_ptr<realtime_tools::RealtimePublisher<control_msgs::JointTrajectoryControllerState>> state_publisher_;
+  int num_hw_joints_;
+  ros::Duration state_publisher_period_;
+  TimeData old_time_data_;
+  realtime_tools::RealtimeBuffer<TimeData> time_data_;
+  ros::Time last_state_publish_time_;
+  std::unique_ptr<realtime_tools::RealtimePublisher<control_msgs::JointTrajectoryControllerState>> state_publisher_;
 };
-}
-
+}  // namespace arm_hybrid_controller
--- arm_hybrid_controller/include/dynamics_interface/dynamics_interface.h
+++ arm_hybrid_controller/include/dynamics_interface/dynamics_interface.h
@@ -5,8 +5,8 @@
 #pragma once
 
 #include <pinocchio/fwd.hpp>
-//must add fwd.hpp before other .h for solve the conflict between ROS and pinocchio
-//https://github.com/wxmerkt/pinocchio_ros_example
+// must add fwd.hpp before other .h for solve the conflict between ROS and pinocchio
+// https://github.com/wxmerkt/pinocchio_ros_example
 #include <pinocchio/parsers/sample-models.hpp>
 #include <pinocchio/algorithm/kinematics.hpp>
 #include <pinocchio/algorithm/dynamics.hpp>
@@ -16,117 +16,120 @@
 
 #include <std_msgs/Float64.h>
 #include <std_msgs/Float64MultiArray.h>
-#include <tools/lp_filter.h>
-#include <tools/ros_param.h>
+#include <arm_common/tools/lp_filter.h>
+#include <arm_common/tools/ros_param.h>
 
 namespace dynamics_interface
 {
 class DynamicsInterface
 {
 public:
-    bool init(ros::NodeHandle &controller_nh);
-    void pubDynamics();
-    void computerInverseDynamics(std::vector<hardware_interface::JointStateHandle> jnt_states);
-    Eigen::VectorXd tau_{},tau_without_a_{},tau_without_a_v_{};
-    Eigen::VectorXd q_{},v_{},last_v_{},a_{},zero_{};
-    bool send_tau_ = false;
+  bool init(ros::NodeHandle& controller_nh);
+  void pubDynamics();
+  void computerInverseDynamics(std::vector<hardware_interface::JointStateHandle> jnt_states);
+  Eigen::VectorXd tau_{}, tau_without_a_{}, tau_without_a_v_{};
+  Eigen::VectorXd q_{}, v_{}, last_v_{}, a_{}, zero_{};
+  bool send_tau_ = false;
 
 private:
-    pinocchio::Model model_;
-    std::string urdf_filename_;
-    pinocchio::Data pinocchio_data_;
+  pinocchio::Model model_;
+  std::string urdf_filename_;
+  pinocchio::Data pinocchio_data_;
 
-    ros::Time last_time_{};
-    ros::NodeHandle node_;
-    ros::Publisher error_pub_,tau_pub_,tau_without_a_pub_,tau_without_a_v_pub_,tau_exe_pub_,a_pub_;
-    std_msgs::Float64MultiArray tau_error_msg_{},tau_msg_{},tau_without_a_msg_{},tau_without_a_v_msg_{},tau_exe_msg_{},a_msg_{};
+  ros::Time last_time_{};
+  ros::NodeHandle node_;
+  ros::Publisher error_pub_, tau_pub_, tau_without_a_pub_, tau_without_a_v_pub_, tau_exe_pub_, a_pub_;
+  std_msgs::Float64MultiArray tau_error_msg_{}, tau_msg_{}, tau_without_a_msg_{}, tau_without_a_v_msg_{},
+      tau_exe_msg_{}, a_msg_{};
 
-    int num_hw_joints_;
-    LowPassFilter* a_lp_filter_;
+  int num_hw_joints_;
+  LowPassFilter* a_lp_filter_;
 };
 
-bool DynamicsInterface::init(ros::NodeHandle &dynamics_nh)
+bool DynamicsInterface::init(ros::NodeHandle& dynamics_nh)
 {
-    if (!dynamics_nh.hasParam("urdf_filename"))
-    {
-        ROS_ERROR_STREAM("NO URDF FILE PATH");
-        return false;
-    }
-    else
-    {
-        dynamics_nh.getParam("urdf_filename",urdf_filename_);
-        pinocchio::urdf::buildModel(urdf_filename_,model_);
-        pinocchio_data_ = pinocchio::Data(model_);
-    }
-    if (!dynamics_nh.hasParam("send_tau"))
-        send_tau_ = true;
-    else
-        dynamics_nh.getParam("send_tau",send_tau_);
-    if (!dynamics_nh.hasParam("num_hw_joints"))
-    {
-        ROS_ERROR_STREAM("NO NUM_HW_JOINTS");
-        return false;
-    }
-    else
-        dynamics_nh.getParam("num_hw_joints",num_hw_joints_);
-//  Init the dynamics relative var.
-    zero_.resize(num_hw_joints_);
-    tau_.resize(num_hw_joints_);
-    tau_without_a_.resize(num_hw_joints_);
-    tau_without_a_v_.resize(num_hw_joints_);
-    q_.resize(num_hw_joints_);
-    v_.resize(num_hw_joints_);
-    last_v_.resize(num_hw_joints_);
-    a_.resize(num_hw_joints_);
-    a_lp_filter_ = new LowPassFilter(dynamics_nh);
+  if (!dynamics_nh.hasParam("urdf_filename"))
+  {
+    ROS_ERROR_STREAM("NO URDF FILE PATH");
+    return false;
+  }
+  else
+  {
+    dynamics_nh.getParam("urdf_filename", urdf_filename_);
+    pinocchio::urdf::buildModel(urdf_filename_, model_);
+    pinocchio_data_ = pinocchio::Data(model_);
+  }
+  if (!dynamics_nh.hasParam("send_tau"))
+    send_tau_ = true;
+  else
+    dynamics_nh.getParam("send_tau", send_tau_);
+  if (!dynamics_nh.hasParam("num_hw_joints"))
+  {
+    ROS_ERROR_STREAM("NO NUM_HW_JOINTS");
+    return false;
+  }
+  else
+    dynamics_nh.getParam("num_hw_joints", num_hw_joints_);
+  //  Init the dynamics relative var.
+  zero_.resize(num_hw_joints_);
+  tau_.resize(num_hw_joints_);
+  tau_without_a_.resize(num_hw_joints_);
+  tau_without_a_v_.resize(num_hw_joints_);
+  q_.resize(num_hw_joints_);
+  v_.resize(num_hw_joints_);
+  last_v_.resize(num_hw_joints_);
+  a_.resize(num_hw_joints_);
+  a_lp_filter_ = new LowPassFilter(dynamics_nh);
 
-//  Init the publisher relative var.
-    tau_error_msg_.data.resize(num_hw_joints_);
-    tau_msg_.data.resize(num_hw_joints_);
-    tau_exe_msg_.data.resize(num_hw_joints_);
-    tau_without_a_msg_.data.resize(num_hw_joints_);
-    tau_without_a_v_msg_.data.resize(num_hw_joints_);
-    a_msg_.data.resize(num_hw_joints_);
-    a_pub_ = dynamics_nh.advertise<std_msgs::Float64MultiArray>("a", 1);
-    error_pub_ = dynamics_nh.advertise<std_msgs::Float64MultiArray>("tau_error", 1);
-    tau_pub_ = dynamics_nh.advertise<std_msgs::Float64MultiArray>("tau", 1);
-    tau_exe_pub_ = dynamics_nh.advertise<std_msgs::Float64MultiArray>("tau_exe", 1);
-    tau_without_a_pub_ = dynamics_nh.advertise<std_msgs::Float64MultiArray>("tau_without_a", 1);
-    tau_without_a_v_pub_ = dynamics_nh.advertise<std_msgs::Float64MultiArray>("tau_without_a_v", 1);
-    return true;
+  //  Init the publisher relative var.
+  tau_error_msg_.data.resize(num_hw_joints_);
+  tau_msg_.data.resize(num_hw_joints_);
+  tau_exe_msg_.data.resize(num_hw_joints_);
+  tau_without_a_msg_.data.resize(num_hw_joints_);
+  tau_without_a_v_msg_.data.resize(num_hw_joints_);
+  a_msg_.data.resize(num_hw_joints_);
+  a_pub_ = dynamics_nh.advertise<std_msgs::Float64MultiArray>("a", 1);
+  error_pub_ = dynamics_nh.advertise<std_msgs::Float64MultiArray>("tau_error", 1);
+  tau_pub_ = dynamics_nh.advertise<std_msgs::Float64MultiArray>("tau", 1);
+  tau_exe_pub_ = dynamics_nh.advertise<std_msgs::Float64MultiArray>("tau_exe", 1);
+  tau_without_a_pub_ = dynamics_nh.advertise<std_msgs::Float64MultiArray>("tau_without_a", 1);
+  tau_without_a_v_pub_ = dynamics_nh.advertise<std_msgs::Float64MultiArray>("tau_without_a_v", 1);
+  return true;
 }
 void DynamicsInterface::pubDynamics()
 {
-//  Pub all data
-    error_pub_.publish(tau_error_msg_);
-    tau_pub_.publish(tau_msg_);
-    tau_exe_pub_.publish(tau_exe_msg_);
-    tau_without_a_pub_.publish(tau_without_a_msg_);
-    tau_without_a_v_pub_.publish(tau_without_a_v_msg_);
-    a_pub_.publish(a_msg_);
+  //  Pub all data
+  error_pub_.publish(tau_error_msg_);
+  tau_pub_.publish(tau_msg_);
+  tau_exe_pub_.publish(tau_exe_msg_);
+  tau_without_a_pub_.publish(tau_without_a_msg_);
+  tau_without_a_v_pub_.publish(tau_without_a_v_msg_);
+  a_pub_.publish(a_msg_);
 }
 void DynamicsInterface::computerInverseDynamics(std::vector<hardware_interface::JointStateHandle> jnt_states)
 {
-    for (int i = 0; i < (int)tau_.size(); ++i) {
-        q_(i) = jnt_states[i].getPosition();
-        v_(i) = jnt_states[i].getVelocity();
-        double original_a = (v_(i)-last_v_[i])/(ros::Time::now()-last_time_).toSec();
-        a_lp_filter_->input(original_a);
-        a_(i) = a_lp_filter_->output();
-        last_v_[i] = v_(i);
-        zero_(i) = 0.;
-    }
-    last_time_ = ros::Time::now();
-//  Use RNEA to computer inverse dynamics
-    tau_ = pinocchio::rnea(model_,pinocchio_data_,q_,v_,a_);
-    tau_without_a_ = pinocchio::rnea(model_,pinocchio_data_,q_,v_,zero_);
-    tau_without_a_v_ = pinocchio::rnea(model_,pinocchio_data_,q_,zero_,zero_);
-    for (int i = 0; i < (int)tau_.size(); ++i) {
-        tau_error_msg_.data[i] = tau_(i) - jnt_states[i].getEffort();
-        tau_msg_.data[i] = tau_(i);
-        tau_without_a_msg_.data[i] = tau_without_a_(i);
-        tau_without_a_v_msg_.data[i] = tau_without_a_v_(i);
-        a_msg_.data[i] = a_(i);
-    }
+  for (int i = 0; i < (int)tau_.size(); ++i)
+  {
+    q_(i) = jnt_states[i].getPosition();
+    v_(i) = jnt_states[i].getVelocity();
+    double original_a = (v_(i) - last_v_[i]) / (ros::Time::now() - last_time_).toSec();
+    a_lp_filter_->input(original_a);
+    a_(i) = a_lp_filter_->output();
+    last_v_[i] = v_(i);
+    zero_(i) = 0.;
+  }
+  last_time_ = ros::Time::now();
+  //  Use RNEA to computer inverse dynamics
+  tau_ = pinocchio::rnea(model_, pinocchio_data_, q_, v_, a_);
+  tau_without_a_ = pinocchio::rnea(model_, pinocchio_data_, q_, v_, zero_);
+  tau_without_a_v_ = pinocchio::rnea(model_, pinocchio_data_, q_, zero_, zero_);
+  for (int i = 0; i < (int)tau_.size(); ++i)
+  {
+    tau_error_msg_.data[i] = tau_(i) - jnt_states[i].getEffort();
+    tau_msg_.data[i] = tau_(i);
+    tau_without_a_msg_.data[i] = tau_without_a_(i);
+    tau_without_a_v_msg_.data[i] = tau_without_a_v_(i);
+    a_msg_.data[i] = a_(i);
+  }
 }
-}
+}  // namespace dynamics_interface
